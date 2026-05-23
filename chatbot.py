7ba0f8b962631ac38b3ef665b713a77c8cf9d8f9
Siddu <mendesiddu9944@gmail.com>
2026-05-23 23:31:52 +0530

    removed api key

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from groq import Groq
 
 app = Flask(__name__)
 
-client = Groq(api_key="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")
+
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 @app.route("/", methods=["GET", "POST"])
 def home():
